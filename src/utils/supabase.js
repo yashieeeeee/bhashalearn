a068285f00add1236423e7fa426057363f377c4b
yashieeeeee <gyashi335@gmail.com>
2026-06-14 21:01:41 +0530

    fix: resolve Vercel build errors

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL = "https://qymebheyxaoqwybqprax.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5bWViaGV5eGFvcXd5YnFwcmF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzNDQ4NTEsImV4cCI6MjA5MTkyMDg1MX0.FK7HQcMaMlghpMpWQqOlToTzYr910T8f30kCMikdDMQ";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
